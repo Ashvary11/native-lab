21a59febab29599111d361180abbbfca6324537e
Ashvary11 <ashvarygidian1996@gmail.com>
2026-09-04 18:40:05 +0530

    checkbox , todos saved added

--- todo/src/app/(tabs)/MyTodos.jsx
+++ todo/src/app/(tabs)/MyTodos.jsx
@@ -1,7 +1,8 @@
 import { useCallback, useState } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Button } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useFocusEffect } from "expo-router";
+import { Checkbox } from "expo-checkbox";
 
 export default function MyTodos() {
   const [todos, setTodos] = useState([]);
@@ -10,6 +11,35 @@ export default function MyTodos() {
     const storedTodos = await AsyncStorage.getItem("todos");
     const todos = storedTodos ? JSON.parse(storedTodos) : [];
     setTodos(todos);
+  };
+  const deleteTodo = async (todoId) => {
+    console.log("todo deleted : ", todoId);
+    const updatedTodos = todos.filter((item) => item.id !== todoId);
+    setTodos(updatedTodos);
+    await AsyncStorage.setItem("todos", JSON.stringify(updatedTodos));
+  };
+
+  const toggleComplete = (todoId) => {
+    const updatedTodos = todos.map((item) => {
+      if (item.id === todoId) {
+        item.completed = !item.completed;
+      }
+
+      return item;
+    });
+    // const updatedTodos = todos.map((item) => {
+    //   if (item.id === todoId) {
+    //     return {
+    //       ...item,
+    //       completed: !item.completed,
+    //     };
+    //   }
+
+    //   return item;
+    // });
+
+    setTodos(updatedTodos);
+    AsyncStorage.setItem("todos", JSON.stringify(updatedTodos));
   };
 
   useFocusEffect(
@@ -34,7 +64,24 @@ export default function MyTodos() {
             }}
           >
             <Text>{index + 1}</Text>
-            <Text>{item.todo}</Text>
+            <Checkbox
+              value={item.completed}
+              onValueChange={() => toggleComplete(item.id)}
+            />
+            <Text
+              style={{
+                flex: 1,
+                color: item.completed ? "gray" : "blue",
+                textDecorationLine: item.completed ? "line-through" : "none",
+              }}
+            >
+              {item.todo}
+            </Text>
+            <Button
+              color="red"
+              title="Delete"
+              onPress={() => deleteTodo(item.id)}
+            />
           </View>
         )}
       />
@@ -42,8 +89,8 @@ export default function MyTodos() {
   );
 }
 
-{
-  /* {todos?.map((item, index) => (
+// {
+/* {todos?.map((item, index) => (
         <View
           key={item.id}
           style={{ display: "flex", flexDirection: "row", gap: 10 }}
@@ -52,4 +99,4 @@ export default function MyTodos() {
           <Text>{item.todo}</Text>
         </View>
       ))} */
-}
+// }
